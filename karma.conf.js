a15e489a16ae923f79bdde6c00aa6a1b6332d3b7
tobisek <jiri@wix.com>
2016-07-03 15:17:31 +0300

    env reporter

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['env'],
 
 
     // web server port
@@ -89,5 +89,6 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
+
   });
 }
